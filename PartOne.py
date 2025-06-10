f6a41386e2ce6e112f84d1dee70f8ccbfa1bef20
Gabriella Nolan <gabnolan@yahoo.co.uk>
2025-06-10 17:25:40 +0100

    Uncommenting commands to check functionality and updated pickle reader to search for "parsed.pickle", added pickle folder to directory

--- PartOne.py
+++ PartOne.py
@@ -13,7 +13,6 @@ import re
 
 nlp = spacy.load("en_core_web_sm")
 nlp.max_length = 2000000
-nltk.download('cmudict')
 
 
 def fk_level(text, d):
@@ -141,16 +140,16 @@ if __name__ == "__main__":
     """
     uncomment the following lines to run the functions once you have completed them
     """
-    #path = Path.cwd() / "p1-texts" / "novels"
-    #print(path)
-    #df = read_novels(path) # this line will fail until you have completed the read_novels function above.
-    #print(df.head())
-    #nltk.download("cmudict")
-    #parse(df)
-    #print(df.head())
-    #print(get_ttrs(df))
-    #print(get_fks(df))
-    #df = pd.read_pickle(Path.cwd() / "pickles" /"name.pickle")
+    path = Path.cwd() / "p1-texts" / "novels"
+    print(path)
+    df = read_novels(path) # this line will fail until you have completed the read_novels function above.
+    print(df.head())
+    nltk.download("cmudict")
+    parse(df)
+    print(df.head())
+    print(get_ttrs(df))
+    print(get_fks(df))
+    df = pd.read_pickle(Path.cwd() / "pickles" /"parsed.pickle")
     # print(adjective_counts(df))
     """ 
     for i, row in df.iterrows():
