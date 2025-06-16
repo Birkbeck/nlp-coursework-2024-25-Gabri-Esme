13628ebd8b2f09607d4d0eeb7bb2815974aac8b2
Gabriella Nolan <gabnolan@yahoo.co.uk>
2025-06-16 17:37:37 +0100

    Question (a) complete and removed intermittend print statements

--- PartTwo.py
+++ PartTwo.py
@@ -9,7 +9,6 @@ df = original_df
 df["party"] = df["party"].str.replace('Labour (Co-op)', 'Labour')
 
 # ii. 
-print(df['party'].value_counts())
 '''Top 4 are: 
 Conservative                        25079
 Labour                               8038
@@ -18,13 +17,13 @@ Scottish National Party              2303
 Liberal Democrat                      803'''
 
 df = df[df['party'].isin(['Conservative', 'Labour', 'Scottish National Party', 'Liberal Democrat'])]
-print(df['party'].unique())
 
 #iii.
 df = df[df['speech_class'].isin(['Speech'])]
-print(df['speech_class'].unique())
 
 #iv.
 df = df[df['speech'].str.len() >= 1000]
+
+print(df.shape)
 
 
