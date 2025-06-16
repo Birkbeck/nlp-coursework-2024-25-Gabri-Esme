505107d6d603c120b090b5677c3d2d711082f9d5
Gabriella Nolan <gabnolan@yahoo.co.uk>
2025-06-16 17:27:23 +0100

    Removed all rows that were not associated to top 4 parties

--- PartTwo.py
+++ PartTwo.py
@@ -2,8 +2,23 @@ import pandas as pd
 
 # Part Two
 # (a)
-# i. 
 original_df = pd.read_csv("p2-texts/hansard40000.csv")
 df = original_df
 
+# i. 
+df["party"] = df["party"].str.replace('Labour (Co-op)', 'Labour')
+
 # ii. 
+print(df['party'].value_counts())
+'''Top 4 are: 
+Conservative                        25079
+Labour                               8038
+Scottish National Party              2303
+(Speaker                               878)
+Liberal Democrat                      803'''
+
+df = df[df['party'].isin(['Conservative', 'Labour', 'Scottish National Party', 'Liberal Democrat'])]
+print(df['party'].unique())
+
+
+
