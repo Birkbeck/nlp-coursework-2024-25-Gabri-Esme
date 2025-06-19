7594b095a28080a0fa085235405ce77c9bec4d62
Gabriella Nolan <gabnolan@yahoo.co.uk>
2025-06-19 01:10:20 +0100

    Implemented custom tokeniser which reached 0.8 accuracy.

--- PartTwo.py
+++ PartTwo.py
@@ -1,9 +1,16 @@
 import pandas as pd
+import re
+import nltk
+import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import f1_score, classification_report
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+nlp = spacy.load('en_core_web_sm')
 
 # Part Two
 # (a)
@@ -84,4 +91,48 @@ svm.fit(X_train, y_train)
 svm_pred = svm.predict(X_test)
 print("SVM Evaluation:", f1_score(y_test, svm_pred, average='macro'))
 print(classification_report(y_test, svm_pred))
+
+# (e)
+stop_words = set(stopwords.words('english'))
+crucial_words = {'scotland', 'scottish', 'independence', 'conservative', 'labour', 'liberal', 'snp', 'tory', 'libdem', 'uk', 'london', 'britain', 'bill', 'parliament', 'tourism'}
+
+def custom_tokenize(text):
+    doc = nlp(text)
+    tokens = []
+    
+    for token in doc:
+        lemma = token.lemma_.lower()
+
+        # Always keep crucial words
+        if lemma in crucial_words:
+            tokens.append(lemma)
+
+        # Replace named entities unless they are crucial
+        elif token.ent_type_ and lemma not in crucial_words:
+            tokens.append(token.ent_type_)
+
+        # Keep clean non-stopword tokens
+        elif token.is_alpha and lemma not in stop_words:
+            tokens.append(lemma)
+
+    return tokens
+
+vect = TfidfVectorizer(tokenizer=custom_tokenize, ngram_range=(1, 3), max_features=3000)
+X = vect.fit_transform(df['speech'])
+y = df['party']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,y,
+    stratify=y,
+    random_state=26 
+)
+
+# SVM
+svm = SVC(kernel='linear', class_weight='balanced')
+svm.fit(X_train, y_train)
+svm_pred = svm.predict(X_test)
+print("SVM Evaluation:", f1_score(y_test, svm_pred, average='macro'))
+print(classification_report(y_test, svm_pred))
+
+# #  (f)
 
